307fea36de3d281c0dde804843f100352ee318c1
Kesper <57568147+kesper02@users.noreply.github.com>
2025-04-14 21:38:00 +0300

    Delete .github/figure_assert.py

--- .github/figure_assert.py
+++
@@ -1,7 +0,0 @@
-class Figure:
-    def __init__(self, type, length) -> None:
-        assert length > 0, "Довжина має бути більшою за 0!"
-        assert type in ["квадрат", "прямокутник", "трикутник"], "Недопустимий тип фігури"
-        self.type = type
-        self.length = length
-12
