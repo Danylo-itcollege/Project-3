b21dc3631eeea62cea85448af65a38c33a5c6e17
Danylo-itcollege <dsenechko2021@itcollege.lviv.ua>
2025-04-15 00:06:30 +0300

    Update test_lab.py

--- test_lab.py
+++ test_lab.py
@@ -1,9 +1,7 @@
-# test_lab.py
-
 from lab import add, subtract
 
 def test_add():
-    assert add(4, 7) == 8
+    assert add(4, 7) == 11  # Changed from 8 to 11 (correct math)
 
 def test_subtract():
-    assert subtract(3, 7) == 3
+    assert subtract(3, 7) == -4  # Changed from 3 to -4 (correct math)
