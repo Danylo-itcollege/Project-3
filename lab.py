16221dd9d7abfb6efa2db9578ce8f49e9ffd6558
Kesper <57568147+kesper02@users.noreply.github.com>
2025-04-14 21:35:33 +0300

    Update lab.py

--- lab.py
+++ lab.py
@@ -1,8 +1,9 @@
-# test_lab.py
-from lab import add, subtract
+# lab.py
+def add(a, b):
+    return a + b
 
-def test_add():
-    assert add(3, 5) == 8
+def subtract(a, b):
+    return a - b
 
-def test_subtract():
-    assert subtract(5, 3) == 2
+if __name__ == "__main__":
+    print(f"3 + 5 = {add(3, 5)}")
